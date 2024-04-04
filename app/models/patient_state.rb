9adaba6c46ae19644721593204b453a48593a49f
FreeWillie265 <wkasakula@gmail.com>
2024-04-04 12:14:32 +0200

    Fix: specify table-name and primary key for patient table

--- app/models/patient_state.rb
+++ app/models/patient_state.rb
@@ -1,4 +1,6 @@
 class PatientState < ApplicationRecord
+  self.table_name = "patient_state"
+  self.primary_key = "patient_state_id"
   belongs_to :patient_state
   belongs_to :patient_program
   belongs_to :site
