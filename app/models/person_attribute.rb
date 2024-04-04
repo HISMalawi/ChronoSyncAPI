a44f044f1dac5886694b8015289e82a08b60dea9
FreeWillie265 <wkasakula@gmail.com>
2024-04-04 12:09:30 +0200

    Fix: specify primary key for person_attribute table

--- app/models/person_attribute.rb
+++ app/models/person_attribute.rb
@@ -1,4 +1,6 @@
 class PersonAttribute < ApplicationRecord
+  self.table_name = "person_attribute"
+  self.primary_key = "person_attribute_id"
   belongs_to :person_attribute
   belongs_to :person
   belongs_to :person_attribute_type
