84ab8aae6abaecb9704b677b060d32668d6a8211
FreeWillie265 <wkasakula@gmail.com>
2024-04-04 16:26:50 +0200

    ADD: define update and delete functions

--- app/controllers/api/v1/operation_controller.rb
+++ app/controllers/api/v1/operation_controller.rb
@@ -2,12 +2,24 @@
 class Api::V1::OperationController < ApplicationController
   before_action :validate_params
   def create
-    render json: {"name": "willie"}
+    #params[:entity]
+    render json: {"status": "creating"}
+  end
+
+  def update
+    render json: {"status": "updating"}
+  end
+
+  def delete
+    render json: {"status": "deleting"}
   end
 
   private
   def validate_params
     params.require([:site_id, :entity])
     render json: {"Error": "Entity #{params[:entity]} not valid"}, status: :bad_request unless ValidationService.validate_entity(params[:entity])
+  #   todo: Add validation for each entity
+    params.permit!
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'operation', to: 'operation#create'
+      put 'operation', to: 'operation#update'
+      delete 'operation', to: 'operation#delete'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
