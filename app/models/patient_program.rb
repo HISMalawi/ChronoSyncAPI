35dae9e4b545a1b142eab745e25c8d17b8c4a493
FreeWillie265 <wkasakula@gmail.com>
2024-04-04 12:15:40 +0200

    Fix: specify table name and primary key for patient_program table

--- app/models/patient_program.rb
+++ app/models/patient_program.rb
@@ -1,4 +1,6 @@
 class PatientProgram < ApplicationRecord
+  self.table_name = "patient_program"
+  self.primary_key = "patient_program_id"
   belongs_to :patient_program
   belongs_to :patient
   belongs_to :program
